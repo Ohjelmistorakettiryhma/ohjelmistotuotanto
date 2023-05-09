4a2649245aea8feddf783af2aab2571da2939930
matildapuhakka <mpuhakka@student.uef.fi>
2023-05-09 16:21:59 +0300

    Lisää toiminnon korjailua, pitäisi toimia, ei jostain syystä vaan tässä minulla.

--- OhjTuotanto1/mokkivaraus/mokivaraus/Form6.cs
+++ OhjTuotanto1/mokkivaraus/mokivaraus/Form6.cs
@@ -29,38 +29,57 @@ namespace mokivaraus
 		{
 			connection.Open();
 
-			string query = "INSERT INTO asiakas (asiakas_id, postinro, etunimi, sukunimi, lahiosoite, puhelinnro, email) VALUES (@asiakas_id, @postinumero, @etunimi, @sukunimi, @lahiosoite, @puhelinnro, @email)";
-			MySqlCommand command = new MySqlCommand(query, connection);
+			// tarkistaa onko postinumero jo olemassa tietokannassa
+			string queryTarkistaPostinro = "SELECT COUNT(*) FROM posti WHERE postinro = @postinro";
+			MySqlCommand commandTarkistaPostinro = new MySqlCommand(queryTarkistaPostinro, connection);
+			commandTarkistaPostinro.Parameters.AddWithValue("@postinro", tbpostinro_asiakas.Text);
+			int count = Convert.ToInt32(commandTarkistaPostinro.ExecuteScalar());
+
+			if (count == 0)
+			{
+				// Postinumeroa ei ole olemassa, lisätään uusi postinumero ja toimipaikka tietokantaan
+				string queryLisaaPostinro = "INSERT INTO posti (postinro, toimipaikka) VALUES (@postinro, @toimipaikka)";
+				MySqlCommand commandLisaaPostinro = new MySqlCommand(queryLisaaPostinro, connection);
+				
+				commandLisaaPostinro.Parameters.AddWithValue("@postinro", tbpostinro_asiakas.Text);
+				commandLisaaPostinro.Parameters.AddWithValue("@toimipaikka", tbkaupunki_asiakas.Text);
+				commandLisaaPostinro.ExecuteNonQuery();
+			}
+
+			// Lisätään uusi asiakasdata asiakastauluun
+			string queryLisaaAsiakas = "INSERT INTO asiakas (asiakasid, postinro, etunimi, sukunimi, lahiosoite, puhelinnro, email) VALUES (@asiakasid, @postinro, @etunimi, @sukunimi, @lahiosoite, @puhelinnro, @email)";
+			MySqlCommand commandLisaaAsiakas = new MySqlCommand(queryLisaaAsiakas, connection);
 
 			int asiakasid;
-			string postinro, etunimi, sukunimi, lahiosoite, puhelinnro, email;
+			string etunimi, sukunimi, postinro, lahiosoite, puhelinnro, email;
 
 			if (int.TryParse(tbasiakasid_asiakas.Text, out asiakasid))
 			{
-				command.Parameters.AddWithValue("@asiakas_id", asiakasid);
-				command.Parameters.AddWithValue("@postinro", tbpostinro_asiakas.Text);
-				command.Parameters.AddWithValue("@etunimi", tbetunimi_asiakas.Text);
-				command.Parameters.AddWithValue("@sukunimi", tbsukunimi_asiakas.Text);
-				command.Parameters.AddWithValue("@lahiosoite", tblahiosoite_asiakas.Text);
-				command.Parameters.AddWithValue("@puhelinnro", tbpuhelinnro_asiakas.Text);
-				command.Parameters.AddWithValue("@email", tbemail_asiakas.Text);
-
-				command.ExecuteNonQuery();
+				commandLisaaAsiakas.Parameters.AddWithValue("@asiakasid", asiakasid);
+				commandLisaaAsiakas.Parameters.AddWithValue("@postinro", tbpostinro_asiakas.Text);
+				commandLisaaAsiakas.Parameters.AddWithValue("@etunimi", tbetunimi_asiakas.Text);
+				commandLisaaAsiakas.Parameters.AddWithValue("@sukunimi", tbsukunimi_asiakas.Text);
+				commandLisaaAsiakas.Parameters.AddWithValue("@lahiosoite", tblahiosoite_asiakas.Text);
+				commandLisaaAsiakas.Parameters.AddWithValue("@puhelinnro", tbpuhelinnro_asiakas.Text);
+				commandLisaaAsiakas.Parameters.AddWithValue("@email", tbemail_asiakas.Text);
+				commandLisaaAsiakas.ExecuteNonQuery();
 
 				connection.Close();
 
 				MessageBox.Show("Asiakas lisätty");
 			}
+
 			else
 			{
 				MessageBox.Show("Väärät syötteet");
 				tbasiakasid_asiakas.Clear();
-				tbpostinro_asiakas.Clear();
 				tbetunimi_asiakas.Clear();
 				tbsukunimi_asiakas.Clear();
+				tbpostinro_asiakas.Clear();
 				tblahiosoite_asiakas.Clear();
 				tbpuhelinnro_asiakas.Clear();
 				tbemail_asiakas.Clear();
+				tbkaupunki_asiakas.Clear();
 			}
 		}
 	}
